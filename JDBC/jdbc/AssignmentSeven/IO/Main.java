3726ca3831f51cd98ff2e09db49be81b5a44660c
hariharan-yeswanth <125393254+hariharan-yeswanth@users.noreply.github.com>
2023-03-22 19:12:49 +0530

    Updated assignment-7

---
+++ JDBC/jdbc/AssignmentSeven/IO/Main.java
@@ -0,0 +1,22 @@
+package jdbc.AssignmentSeven.IO;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        FileInputStream fileInputStream = new FileInputStream(
+                "/Users/hariharansaravanan/Documents/JAVA/Learning/JDBC/jdbc/AssignmentSeven/IO/Input.txt");
+        FileOutputStream fileOutputStream = new FileOutputStream(
+                "/Users/hariharansaravanan/Documents/JAVA/Learning/JDBC/jdbc/AssignmentSeven/IO/Output.txt");
+        int numberOfCharactersRead = 0;
+        byte buffer[] = new byte[8];
+        while ((numberOfCharactersRead = fileInputStream.read(buffer)) != -1) {
+            String string = new String(buffer, 0, numberOfCharactersRead);
+            System.out.println(string);
+            fileOutputStream.write(buffer, 0, numberOfCharactersRead);
+        }
+        fileInputStream.close();
+        fileOutputStream.close();
+    }
+}
