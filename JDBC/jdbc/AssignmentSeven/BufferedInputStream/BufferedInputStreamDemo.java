c952a0e5bfaf04e7257a50cac29c9c73af1bae43
hariharan-yeswanth <125393254+hariharan-yeswanth@users.noreply.github.com>
2023-03-22 18:40:02 +0530

    Uploaded assignment-7

---
+++ JDBC/jdbc/AssignmentSeven/BufferedInputStream/BufferedInputStreamDemo.java
@@ -0,0 +1,23 @@
+package jdbc.AssignmentSeven.BufferedInputStream;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+
+public class BufferedInputStreamDemo {
+    public static void main(String args[]) {
+        try {
+            FileInputStream fin = new FileInputStream(
+                    "/Users/hariharansaravanan/Documents/JAVA/Learning/JDBC/jdbc/AssignmentSeven/BufferedInputStream/testout.text");
+            BufferedInputStream bin = new BufferedInputStream(fin);
+            int i;
+            while ((i = bin.read()) != -1) {
+                System.out.print((char) i);
+            }
+            bin.close();
+            fin.close();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+}
---
+++ JDBC/jdbc/AssignmentSeven/SequenceInputStream/InputStreamExample.java
@@ -0,0 +1,21 @@
+package jdbc.AssignmentSeven.SequenceInputStream;
+
+import java.io.FileInputStream;
+import java.io.SequenceInputStream;
+
+class InputStreamExample {
+    public static void main(String args[]) throws Exception {
+        FileInputStream input1 = new FileInputStream(
+                "/Users/hariharansaravanan/Documents/JAVA/Learning/JDBC/jdbc/AssignmentSeven/SequenceInputStream/testin.txt");
+        FileInputStream input2 = new FileInputStream(
+                "/Users/hariharansaravanan/Documents/JAVA/Learning/JDBC/jdbc/AssignmentSeven/SequenceInputStream/testout.txt");
+        SequenceInputStream inst = new SequenceInputStream(input1, input2);
+        int j;
+        while ((j = inst.read()) != -1) {
+            System.out.print((char) j);
+        }
+        inst.close();
+        input1.close();
+        input2.close();
+    }
+}
